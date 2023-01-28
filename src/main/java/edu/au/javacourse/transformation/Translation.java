bc86365fe724246fe9f2e03aed62368f405c21d7
buyanthg <62413239+buyanthg@users.noreply.github.com>
2023-01-28 13:12:19 +0300

    Update Translation.java

--- src/main/java/edu/au/javacourse/transformation/Translation.java
+++ src/main/java/edu/au/javacourse/transformation/Translation.java
@@ -1,8 +1,11 @@
 public class Translation extends AffineTrasformation{
     private static Matrix translationMatrix(double dx, double dy){
-        double[][] translation = {{1, 0, 0},
-                                {0, 1, 0},
-                                {dx, dy, 1}};
+        double[][] translation = double[3][3];
+        for (int i = 0, i < 3, i++){
+            translation[i][i] = 1
+        }
+        translation[2][0] = dx;
+        translation[2][1] = dy;
         return new Matrix(translation);
     }
     public Translation(double dx, double dy){
